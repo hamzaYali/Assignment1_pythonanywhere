80df4d2ee7c55ae9709db1dce5d479ed7416ab7a
hamzaYali <98500914+hamzaYali@users.noreply.github.com>
2024-09-24 14:00:42 -0500

    added import

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from django.contrib import admin
+
 
 
 urlpatterns = [
